7fffc43d7b6d29c0633612b26bf23e3f472d289d
LamMun <lammun021100@gmail.com>
2022-06-27 09:46:25 +0200

    add con kak

--- src/components/PlanOfStudy.js
+++ src/components/PlanOfStudy.js
@@ -7,7 +7,7 @@ import PickedRowUU from './PickedRowUU';
 import AddRow from './AddRow';
 
 
-//Tak nepotrebuji this component - RowAdd
+//Tak nepotrebuji this component - RowAdd add con kak
 //nameSubjects : 0-Informatika, 1-Math, 2-Teorie Graf, 3-English
 // function RowAdd({nameSubject,handleDataAdd,handleDataAddConfirm}){ 
 //    if(nameSubject===0){ 
